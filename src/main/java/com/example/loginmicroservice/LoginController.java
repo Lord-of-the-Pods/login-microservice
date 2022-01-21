e2a2ea05933a43281f86108d2f630e3760e0731d
Abhishek Vishnoi <vishnoi.abhishek@hotmail.com>
2022-01-21 07:44:33 +0000

    Updated LoginController.java

--- src/main/java/com/example/loginmicroservice/LoginController.java
+++ src/main/java/com/example/loginmicroservice/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-//@CrossOrigin
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class LoginController {

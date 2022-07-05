45f976a6c0c78233fcfc246ae7c2ba0af2670652
arushi <arushi.goyal@hotmail.com>
2022-07-05 20:45:35 +0530

    change password message updated

--- src/main/java/com/example/loginmicroservice/LoginController.java
+++ src/main/java/com/example/loginmicroservice/LoginController.java
@@ -109,13 +109,10 @@ public class LoginController {
     @PostMapping("/changePassword")
     public String changePassword(@RequestBody ChangePassword password){
         logger.info("a request received to login a user..!!");
-
             if((password.getOldPassword().equals(password.getNewPassword()))) {
                 logger.info("password change is successful");
-                return "password not changed";
+                return "password not changed data";
             }
         return "password changed";
     }
-
-
 }
